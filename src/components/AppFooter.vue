49ae95e582db181d9cc27ebe1800205a8de69be0
Yuri-Punzo <yuripunzo@hotmail.it>
2023-07-25 18:35:27 +0200

    home page wip

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,8 +6,14 @@ export default {
 
 <template>
     <div class="text-light my_footer">
-        <div class="container h-100 d-flex align-items-center">
-            <p class="mb-0"><i class="fa-regular fa-copyright"></i> Copyrights 2023 Yuri Punzo - All Rights Reserved</p>
+        <div class="container h-100 d-flex align-items-center justify-content-between">
+            <p class="mb-0">
+                <i class="fa-regular fa-copyright"></i> Copyrights 2023 Yuri Punzo - All Rights Reserved
+            </p>
+            <div>
+                <i class="fa-brands fa-linkedin ms-3"></i>
+                <i class="fa-brands fa-github ms-3"></i>
+            </div>
         </div>
     </div>
 </template>
