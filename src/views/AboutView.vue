43858bbf827aa5e29173ee85d9114228b731ddce
Yuri-Punzo <yuripunzo@hotmail.it>
2023-07-25 14:46:42 +0200

    bg colors

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -15,15 +15,23 @@ export default {
 </script>
 
 <template>
-    <div class="container">
+    <div class="background">
+        <div class="container text-light">
 
-        <h1 class="pt-5">About</h1>
-        <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque autem, officiis id expedita non architecto.
-                Mollitia repellat sit eaque voluptates assumenda.</p>
+            <h1 class="pt-5">About</h1>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque autem, officiis id expedita non
+                    architecto.
+                    Mollitia repellat sit eaque voluptates assumenda.</p>
+            </div>
         </div>
     </div>
 </template>
 
 
-<style lang="scss"></style>
+<style lang="scss">
+.background {
+    height: 100%;
+    background-color: #212529;
+}
+</style>
--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -15,34 +15,36 @@ export default {
 </script>
 
 <template>
-
-    <div class="container">
-
-        <h1 class="pt-5">Contacts</h1>
-        <div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dolore repellat ipsum perferendis
-                deleniti, pariatur id vel voluptatem quam tenetur eos quas sint facere? Facere aspernatur inventore
-                eaque molestiae tempore! Eaque adipisci est quo nesciunt ab repellat veniam reprehenderit consequatur
-                dolorem voluptates quasi, nostrum explicabo esse nihil ex, at id aspernatur voluptatem temporibus,
-                voluptate sint a nobis fugit accusantium! Vitae laboriosam aut nobis facilis, maiores voluptatibus in
-                asperiores beatae repellendus rerum! Quos expedita accusantium nemo praesentium magnam, accusamus in
-                dolor corporis at nam. Quod incidunt repellendus possimus! Laudantium dignissimos esse ipsam harum
-                officiis veniam illo! Consequuntur nobis doloribus unde quasi alias, ducimus natus. Ut voluptate nisi
-                rem quos commodi sed numquam laboriosam repellendus expedita hic esse, amet aperiam vitae repudiandae
-                eaque fugiat nobis possimus soluta quibusdam explicabo culpa animi cupiditate minima. Non ipsa
-                doloremque voluptatibus, temporibus tempore impedit cupiditate possimus rem dolores sequi voluptate?
-                Quia, ipsa? Porro cumque accusamus, itaque dolorum pariatur ad eligendi neque fuga laborum illo
-                voluptas. Quibusdam enim atque laboriosam unde minus nemo iusto consequuntur voluptatum reprehenderit
-                harum assumenda sint earum libero omnis obcaecati ut error, voluptate nesciunt maxime ullam. Beatae
-                architecto veritatis facere est labore? Necessitatibus sunt dignissimos, quasi velit nam incidunt!
-                Excepturi sunt rerum illo. Corrupti voluptates, consectetur provident officia eaque necessitatibus rem.
-                Aliquam, mollitia?</p>
+    <div class="background">
+        <div class="container">
+            <h1 class="pt-5">Contacts</h1>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dolore repellat ipsum perferendis
+                    deleniti, pariatur id vel voluptatem quam tenetur eos quas sint facere? Facere aspernatur inventore
+                    eaque molestiae tempore! Eaque adipisci est quo nesciunt ab repellat veniam reprehenderit consequatur
+                    dolorem voluptates quasi, nostrum explicabo esse nihil ex, at id aspernatur voluptatem temporibus,
+                    voluptate sint a nobis fugit accusantium! Vitae laboriosam aut nobis facilis, maiores voluptatibus in
+                    asperiores beatae repellendus rerum! Quos expedita accusantium nemo praesentium magnam, accusamus in
+                    dolor corporis at nam. Quod incidunt repellendus possimus! Laudantium dignissimos esse ipsam harum
+                    officiis veniam illo! Consequuntur nobis doloribus unde quasi alias, ducimus natus. Ut voluptate nisi
+                    rem quos commodi sed numquam laboriosam repellendus expedita hic esse, amet aperiam vitae repudiandae
+                    eaque fugiat nobis possimus soluta quibusdam explicabo culpa animi cupiditate minima. Non ipsa
+                    doloremque voluptatibus, temporibus tempore impedit cupiditate possimus rem dolores sequi voluptate?
+                    Quia, ipsa? Porro cumque accusamus, itaque dolorum pariatur ad eligendi neque fuga laborum illo
+                    voluptas. Quibusdam enim atque laboriosam unde minus nemo iusto consequuntur voluptatum reprehenderit
+                    harum assumenda sint earum libero omnis obcaecati ut error, voluptate nesciunt maxime ullam. Beatae
+                    architecto veritatis facere est labore? Necessitatibus sunt dignissimos, quasi velit nam incidunt!
+                    Excepturi sunt rerum illo. Corrupti voluptates, consectetur provident officia eaque necessitatibus rem.
+                    Aliquam, mollitia?</p>
+            </div>
         </div>
     </div>
-
 </template>
 
 
 <style lang="scss">
-
+.background {
+    height: 100%;
+    background-color: #212529;
+}
 </style>
