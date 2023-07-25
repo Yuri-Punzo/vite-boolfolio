54339fad4ca2b0f2133f09c67ae7b5b41e33fce7
Yuri-Punzo <yuripunzo@hotmail.it>
2023-07-25 19:09:31 +0200

    wip

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -19,32 +19,26 @@ export default {
         <div class="container text-light">
             <h1 class="pt-5">Contacts</h1>
             <div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dolore repellat ipsum perferendis
-                    deleniti, pariatur id vel voluptatem quam tenetur eos quas sint facere? Facere aspernatur inventore
-                    eaque molestiae tempore! Eaque adipisci est quo nesciunt ab repellat veniam reprehenderit consequatur
-                    dolorem voluptates quasi, nostrum explicabo esse nihil ex, at id aspernatur voluptatem temporibus,
-                    voluptate sint a nobis fugit accusantium! Vitae laboriosam aut nobis facilis, maiores voluptatibus in
-                    asperiores beatae repellendus rerum! Quos expedita accusantium nemo praesentium magnam, accusamus in
-                    dolor corporis at nam. Quod incidunt repellendus possimus! Laudantium dignissimos esse ipsam harum
-                    officiis veniam illo! Consequuntur nobis doloribus unde quasi alias, ducimus natus. Ut voluptate nisi
-                    rem quos commodi sed numquam laboriosam repellendus expedita hic esse, amet aperiam vitae repudiandae
-                    eaque fugiat nobis possimus soluta quibusdam explicabo culpa animi cupiditate minima. Non ipsa
-                    doloremque voluptatibus, temporibus tempore impedit cupiditate possimus rem dolores sequi voluptate?
-                    Quia, ipsa? Porro cumque accusamus, itaque dolorum pariatur ad eligendi neque fuga laborum illo
-                    voluptas. Quibusdam enim atque laboriosam unde minus nemo iusto consequuntur voluptatum reprehenderit
-                    harum assumenda sint earum libero omnis obcaecati ut error, voluptate nesciunt maxime ullam. Beatae
-                    architecto veritatis facere est labore? Necessitatibus sunt dignissimos, quasi velit nam incidunt!
-                    Excepturi sunt rerum illo. Corrupti voluptates, consectetur provident officia eaque necessitatibus rem.
-                    Aliquam, mollitia?</p>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus quam quasi necessitatibus
+                    doloribus illo accusantium asperiores consequatur voluptatem veniam? Inventore nesciunt quisquam
+                    voluptates obcaecati. Labore soluta blanditiis exercitationem. Porro, sit? Ipsum quibusdam quae deserunt
+                    accusantium ullam, cupiditate quas similique dolore fugit, eius necessitatibus harum non magnam
+                    voluptatem quam nihil cumque quod? Velit repudiandae dolore inventore neque sequi nesciunt quae
+                    consequatur delectus voluptatum debitis qui voluptatem quo doloribus veritatis possimus optio cumque
+                    voluptates nihil cupiditate rerum repellat iste, iure odit praesentium. Nihil quod odit debitis
+                    assumenda tempore corporis beatae sed unde architecto numquam. Debitis sint eveniet dolores, tempore
+                    nostrum, ex eum aliquid officia iste similique animi fugit dolorum ea a! Incidunt veniam nostrum commodi
+                    voluptas totam, quibusdam animi fuga labore sunt consectetur possimus repellat corrupti ducimus modi?
+                    Nemo, quibusdam unde! Facere esse quasi eos tempore doloremque ullam commodi laborum cumque explicabo
+                    eligendi quas omnis consequatur dolorum dolore ex, a architecto illum.
+                </p>
             </div>
         </div>
-    </div>
-</template>
+</div></template>
 
 
-<style lang="scss" scoped>
-.background {
+<style lang="scss" scoped>.background {
     height: 100%;
     background-color: #212529;
-}
-</style>
+}</style>
